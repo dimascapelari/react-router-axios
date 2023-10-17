44059dcf1f63ee90039041742a2573016b79f575
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-10-16 21:43:59 -0300

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div>
-        <h1>React Axios</h1>
+      <div className="container">
+        <Outlet />
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
+// páginas
+import { Home } from "./routes/Home";
+import { NewPost } from "./routes/NewPost";
+
 const router = createBrowserRouter([
   {
     element: <App />,
     children: [
       {
         path: "/",
+        element: <Home />,
       },
       {
         path: "/new",
+        element: <NewPost />,
       },
     ],
   },
@@ -19,6 +25,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
