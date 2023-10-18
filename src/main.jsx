97a40979740d9aa76d1efa45da13cb0c5d009c9c
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-10-18 09:14:11 -0300

    React Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import "./index.css";
 
 // páginas
 import { Home } from "./routes/Home";
