31dc5e04b3e9cef126e10648cf69ddcec9c44875
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-10-18 09:44:52 -0300

    React Router Axios

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,3 +1,27 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./Home.css";
+
 export const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+
+      const data = response.data;
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return <div>Home</div>;
 };
